3a5335eed1b9e1d0552ccb0225c5083004704bea
RafaelBochiNetcred <rafael.barros@netcredbrasil.com.br>
2026-06-19 17:54:52 -0300

    Update

--- webhooks/docs/list.py
+++ webhooks/docs/list.py
@@ -4,7 +4,7 @@ from webhooks.serializers import WebhookSerializer
 
 
 WEBHOOK_LIST_SCHEMA = {
-    "summary": "Lista assinaturas de webhook",
+    "summary": "Lista assinaturas de webhook aaa"
     "description": (
         "Retorna as assinaturas cadastradas. Permite filtrar por empresa, "
         "status, URL de destino e eventos assinados. Use este endpoint para "
